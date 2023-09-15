8f921d8f4df1aa70f5248a8dd327c24b0927647b
Raymond Oluoch <rasholuoch@gmail.com>
2023-09-15 22:17:26 +0300

    Revert "completed user and auth modules"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
-import { config } from 'dotenv';
-config();
-
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import typeOrmConfig from './typeorm.config';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
